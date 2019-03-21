9be784eb43fafd42e3878c5cf4cac441f7b4be67
小罗 <1071865953@qq.com>
2019-03-21 16:07:51 +0800

    '增加了公司信息页面方法,进入公司页面网址输入company/ ' --来自小罗

--- training/admin.py
+++ training/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Company
 
 # Register your models here.
+"""
+公司后台admin,
+后台显示字段为公司名称'name'和公司简介'info'
+"""
+
+
+@admin.register(Company)
+class CompanyAdmin(admin.ModelAdmin):
+    # 后台显示字段name,info
+    list_display = ('name', 'info',)
--- training/urls.py
+++ training/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    # 公司信息详情页__来自小罗
+    path('company/', views.company, name='company')
 
 ]
