f4f461df3f1bd5c2f536e430a3b3f77ba31d588d
王凯杰 <l>
2019-03-21 18:54:17 +0800

    查看 修改 增加

--- training/urls.py
+++ training/urls.py
@@ -5,7 +5,6 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('post/update/<int:id>/', views.post_update, name='post_update'),
     path('post_add/',views.post_add,name='post_add'),
-    path('post/<int:id>/', views.post_list,name='post_list'),
     path('post/detail/<int:id>/',views.post_detail,name='post_detail')
 
 ]
