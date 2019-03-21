d747a3c37ae3f8ad870aac4d97e37ad41d66dbce
小白 <2437096149@qq.com>
2019-03-21 20:28:27 +0800

    company

--- training/urls.py
+++ training/urls.py
@@ -3,9 +3,9 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('post_add/',views.post_add,name='post_add'),
-    path('post/detail/<int:id>/',views.post_detail,name='post_detail'),
-    path('post/update/<int:id>/',views.post_update,name='post_update'),
+    path('post_add/', views.post_add, name='post_add'),
+    path('post/detail/<int:id>/', views.post_detail, name='post_detail'),
+    path('post/update/<int:id>/', views.post_update, name='post_update'),
     path('post_delete/<int:id>/', views.post_delete, name='post_delete'),
-
+    path('company/', views.company, name='company'),
 ]
