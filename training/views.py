617ad945e25c81dddde6ee77d0fa86236664187f
王凯杰 <l>
2019-03-22 15:58:26 +0800

    公告，部门以及班级

--- training/views.py
+++ training/views.py
@@ -20,7 +20,7 @@ def company(request):
 
 def post_detail(request, id):
     post = Post.objects.get(id=id)
-    return render(request, 'training/post_detail.html', {'post': post})
+    return render(request, 'training/post/post_detail.html', {'post': post})
 
 
 def post_add(request):
@@ -38,7 +38,7 @@ def post_add(request):
                 return redirect("index")  # 返回主界面
         else:
             post_add = PostForm()  # 否则实例化对象
-            return render(request, 'training/post_add.html', {'post_add': post_add})
+            return render(request, 'training/post/post_add.html', {'post_add': post_add})
     else:
         return HttpResponseRedirect("不能进行增加！！！")
 
@@ -57,7 +57,7 @@ def post_update(request, id):
                 return redirect("index")  # 返回主界面
         else:
             form = PostForm(instance=post)  # 否则实例化对象
-            return render(request, 'training/post_update.html', {'form': form})
+            return render(request, 'training/post/post_update.html', {'form': form})
     else:
         return HttpResponseRedirect("非职工身份不能修改公告！")
 
@@ -78,7 +78,7 @@ def post_delete(request, id):
 def profile_list(request, id):
     department = Department.objects.get(id=id)
     profiles = department.depart_emp.all()
-    return render(request, 'training/department_detail.html', {'department': department, 'profiles': profiles})
+    return render(request, 'training/department/department_detail.html', {'department': department, 'profiles': profiles})
 
 
 #班级测试，艾鹏
@@ -89,7 +89,7 @@ def profile_list(request, id):
 # 查看部门__斌
 def section_list(request):
     look = Department.objects.filter(name__contains="部")
-    return render(request, 'training/look_section.html', {'look': look})
+    return render(request, 'training/department/look_section.html', {'look': look})
 
 
 
@@ -98,13 +98,13 @@ def section_list(request):
 # 部门下的人员__斌
 def section_details(request, id):
     sec = Department.objects.get(id=id)
-    return render(request, 'training/section_details.html', {'sec': sec})
+    return render(request, 'training/department/section_details.html', {'sec': sec})
 
 
 # 班级列表
 def class_list(request):
     department = Department.objects.filter(name__contains="班")
-    return render(request, 'training/class_list.html', {'department': department})
+    return render(request, 'training/department/class_list.html', {'department': department})
 
 
 # 班级详细介绍
@@ -112,4 +112,4 @@ def class_detail(request, class_id):
     class_details = Department.objects.get(id=class_id)
     name = class_details.name
     info = class_details.info
-    return render(request, 'training/class_detail.html', {'class_details': class_details, 'name': name, 'info': info})
+    return render(request, 'training/department/class_detail.html', {'class_details': class_details, 'name': name, 'info': info})
