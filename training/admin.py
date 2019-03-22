3cf110bbd0721d3ee677347c632fb4177c9b3907
王凯杰 <l>
2019-03-22 10:55:36 +0800

    修改-查看-增加-删除

--- training/admin.py
+++ training/admin.py
@@ -1,23 +1,39 @@
 from django.contrib import admin
-from .models import Post,Profile,Position,Department,Company
+from .models import Post, Profile, Position, Department, Company, Note, Duty
+
+
 # Register your models here.
 
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
-    list_display = ('title','content')
+    list_display = ('title', 'content')
+
 
 @admin.register(Profile)
 class ProfileAdmin(admin.ModelAdmin):
-    list_display = ('name','job')
+    list_display = ('name', 'job')
+
 
 @admin.register(Position)
 class PositionAdmin(admin.ModelAdmin):
-    list_display = ('name','info')
+    list_display = ('name', 'info')
+
 
 @admin.register(Department)
 class CompanyAdmin(admin.ModelAdmin):
-    list_display = ('name','info','company')
+    list_display = ('name', 'info', 'company')
+
 
 @admin.register(Company)
 class CompanyAdmin(admin.ModelAdmin):
-    list_display = ('name','info',)
+    list_display = ('name', 'info',)
+
+
+@admin.register(Note)
+class NoteAdmin(admin.ModelAdmin):
+    list_display = ('starttime', 'endtime', 'profile')
+
+
+@admin.register(Duty)
+class DutyAdmin(admin.ModelAdmin):
+    list_display = ('name',)
